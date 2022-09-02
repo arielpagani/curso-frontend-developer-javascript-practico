4050b088b1d045dace5d9d468e11fb1347f0d439
arielpagani <arielpagani@outlook.com>
2022-09-02 06:51:35 -0300

    Modificacion del mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerMenu = document.querySelector(`.menu`);
+const mobileMenu = document.querySelector(`.mobile-menu`);
 
 navbarEmail.addEventListener('click', toogleDesktopMenu);
 
 function toogleDesktopMenu() {
-  desktopMenu.classList.toggle('desktop-menu-inactive')
+  desktopMenu.classList.toggle('inactive');
+}
+burguerMenu.addEventListener("click", toogleMobileMenu);
+
+function toogleMobileMenu() {
+  mobileMenu.classList.toggle(`inactive`);
 }
